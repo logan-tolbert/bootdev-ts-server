574e05d27a3d31bfd2d3a4637cec034b6c2fad79
Logan Tolbert <lodev205@gmail.com>
2025-08-12 15:49:26 -0500

    ✨ Add middleware and route handlers
    - Create logging and metrics middlware
    - Create routes and handlers for health check and basic request metrics

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,46 @@
-import express from "express";
-import path from "path";
+import express, { NextFunction, Request, Response } from 'express';
+import { config } from './config.js';
 
 const app = express();
 const PORT = 8080;
 
-app.use(express.static("."));
-
+app.use(middlewareLogResponse);
+app.use('/app', middlewareMetricsInc, express.static('./src/app'), );
+app.get('/api/healthz', handlerReadiness);
+app.get('/api/metrics', handlerRequestCounter);
+app.get('/api/reset', handlerResetRequestCounter);
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
+
+function handlerReadiness(req:Request, res:Response):void{
+  res.set('Content-Type', 'text/plain;charset=utf-8');
+  res.status(200).send("OK");
+};
+
+function handlerRequestCounter(req:Request, res: Response):void{
+  res.set('Content-Type', 'text/plain; charset=utf-8');
+  res.status(200).send(`Hits: ${config.fileserverHits}`);
+}
+
+function handlerResetRequestCounter(req:Request, res: Response):void{
+  res.set('Content-Type', 'text/plain; charset=utf-8');
+  config.fileserverHits = 0;
+  res.status(200).send(`Hits: ${config.fileserverHits}`);
+}
+
+function middlewareLogResponse(req:Request, res:Response, next: NextFunction){
+  res.on("finish", () => {
+    if(res.statusCode !== 200){
+      console.log(`[NON-OK] ${req.method} ${req.url} - Status: ${res.statusCode}`)
+    }
+  });
+  next();
+};
+
+function middlewareMetricsInc(req: Request, res: Response, next: NextFunction){
+  res.on("finish", () => {
+    config.fileserverHits++;
+  });
+  next();
+}
