c49ea9eb6a8c4708a015c454f7aa9bc7461e7ebe
Logan Tolbert <lodev205@gmail.com>
2025-08-13 08:09:19 -0500

    ✨ Add endpoint and centralized error handling
    - Implement handlerValidateChirp to limit chirp length to 140 characters and filter disallowed words
    - Add custom error classes for Bad Request, Unauthenticated, Unauthorized, and Not Found scenarios
    - Create centralized error-handling middleware to process custom errors
    - Refactor existing handler functions to use async/await
    - Update request pipeline to use promises for passing errors to middleware

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,36 @@
 import express, { NextFunction, Request, Response } from 'express';
 import { config } from './config.js';
+import { BadRequestError, NotFoundError, UserForbiddenError, UserNotAuthenticatedError } from './errors.js';
 
 const app = express();
 const PORT = 8080;
 
 app.use(middlewareLogResponse);
+app.use(express.json());
 app.use('/app', middlewareMetricsInc, express.static('./src/app'), );
-app.get('/api/healthz', handlerReadiness);
-app.get('/admin/metrics', handlerRequestCounter);
-app.post('/admin/reset', handlerResetRequestCounter);
+app.get('/api/healthz', (req,res,next) => {
+  Promise.resolve(handlerReadiness(req,res).catch(next));
+});
+app.get('/admin/metrics', (req,res,next)=>{
+  Promise.resolve(handlerRequestCounter(req,res).catch(next));
+});
+app.post('/admin/reset', (req,res,next) => {
+  Promise.resolve(handlerResetRequestCounter(req,res).catch(next));
+});
+app.post('/api/validate_chirp', (req,res,next) => {
+  Promise.resolve(handlerValidateChirp(req,res).catch(next));
+});
+app.use(middlewareErrorHandler);
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
 
-function handlerReadiness(req:Request, res:Response):void{
+async function handlerReadiness(req:Request, res:Response){
   res.set('Content-Type', 'text/text; charset=utf-8');
   res.status(200).send("OK");
 };
 
-function handlerRequestCounter(req:Request, res: Response):void{
+async function handlerRequestCounter(req:Request, res: Response){
   res.set('Content-Type', 'text/html; charset=utf-8');
   res.status(200)
   res.write(`
@@ -32,10 +44,52 @@ function handlerRequestCounter(req:Request, res: Response):void{
   res.send();
 }
 
-function handlerResetRequestCounter(req:Request, res: Response):void{
+async function handlerResetRequestCounter(req:Request, res: Response){
   res.set('Content-Type', 'text/plain; charset=utf-8');
   config.fileserverHits = 0;
   res.status(200).send(`Hits: ${config.fileserverHits}`);
+}
+
+async function handlerValidateChirp(req: Request, res: Response) {
+  const maxChirpLength = 140;
+  type RequestParameters = {
+    body: string;
+  };
+
+  let body = "";
+
+  req.on("data", (chunk) => {
+    body += chunk;
+  });
+
+  let reqParams: RequestParameters;
+ 
+  reqParams = req.body
+  
+  if (reqParams.body.length > maxChirpLength) {
+     throw new BadRequestError(
+      `Chirp is too long. Max length is ${maxChirpLength}`,
+  );
+  }
+    
+  const words = reqParams.body.split(" ");
+
+  const badWords = ["kerfuffle", "sharbert", "fornax"];
+  for (let i = 0; i < words.length; i++) {
+    const word = words[i];
+    const loweredWord = word.toLowerCase();
+    if (badWords.includes(loweredWord)) {
+        words[i] = "****";
+    }
+  }
+
+  const cleaned = words.join(" ");
+  res.header("Content-Type", "application/json");
+  const responseBody = JSON.stringify({
+      cleanedBody: cleaned,
+  });
+  res.status(200).send(responseBody);
+  res.end();
 }
 
 function middlewareLogResponse(req:Request, res:Response, next: NextFunction){
@@ -52,4 +106,36 @@ function middlewareMetricsInc(req: Request, res: Response, next: NextFunction){
     config.fileserverHits++;
   });
   next();
+}
+
+function middlewareErrorHandler(
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+   let statusCode = 500;
+  let message = "Something went wrong on our end";
+
+  if (err instanceof BadRequestError) {
+    statusCode = 400;
+    message = err.message;
+  } else if (err instanceof UserNotAuthenticatedError) {
+    statusCode = 401;
+    message = err.message;
+  } else if (err instanceof UserForbiddenError) {
+    statusCode = 403;
+    message = err.message;
+  } else if (err instanceof NotFoundError) {
+    statusCode = 404;
+    message = err.message;
+  }
+
+  if (statusCode >= 500) {
+    console.log(err.message);
+  }
+  res.header("Content-Type", "application/json");
+  const body = JSON.stringify({error: message});
+  res.status(statusCode).send(body);
+  res.end();
 }
